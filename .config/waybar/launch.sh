875f784b03054989723d2448e76f716203162713
entooone <entooone@gmail.com>
2020-06-08 16:50:10 +0900

    Add waybar config

---
+++ .config/waybar/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q waybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x waybar >/dev/null; do sleep 1; done
+
+# Launch Waybar, using default config location ~/.config/waybar/config
+waybar &
+
+echo "Waybar launched..."
