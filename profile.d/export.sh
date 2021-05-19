eaa774444b6f2049ecee502920b8e39ef4abffa6
entooone <entooone@gmail.com>
2021-05-20 00:36:40 +0900

    sh: Update enviroment values

--- profile.d/export.sh
+++ profile.d/export.sh
@@ -2,7 +2,7 @@ export CLICOLOR=true
 export LSCOLORS='exfxcxdxbxGxDxabagacad'
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
 export EDITOR=/usr/bin/vim
-export BROWSER=/usr/bin/firefox
+export BROWSER=/usr/bin/chromium
 export SCREENDIR=$HOME/.screen
 export TERM=xterm-256color
 export PATH=$PATH:$HOME/.local/bin
@@ -13,6 +13,10 @@ export FZF_DEFAULT_OPTS='--preview "if [ -d {} ]; then ls -1F --color {}/; else 
 if type bat &> /dev/null; then
     export FZF_DEFAULT_OPTS='--preview "if [ -d {} ]; then ls -1F --color {}/; else bat --style=numbers --color=always --line-range :500 {}; fi"'
 fi
+
+# deno
+#─────────────────────────────
+export PATH="/home/u5er/.deno/bin:$PATH"
 
 
 # golang
