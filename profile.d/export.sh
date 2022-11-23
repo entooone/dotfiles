2520536876621c7e82d0a886a77697c291e3b38f
u5er <u5er@fedora.tail76dd.ts.net>
2022-11-23 12:41:27 +0900

    profile: Add .cargo to PATH

--- profile.d/export.sh
+++ profile.d/export.sh
@@ -2,7 +2,6 @@ export CLICOLOR=true
 export LSCOLORS='exfxcxdxbxGxDxabagacad'
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
 export EDITOR=/usr/bin/vim
-export BROWSER=/usr/bin/chromium
 export SCREENDIR=$HOME/.screen
 export TERM=xterm-256color
 export PATH=$PATH:$HOME/.local/bin
@@ -20,6 +19,10 @@ export PATH="/home/u5er/.deno/bin:$PATH"
 #─────────────────────────────
 if type "go" > /dev/null 2>&1; then
     export PATH=$PATH:$(go env GOPATH)/bin
+fi
+
+if type "cargo" > /dev/null 2>&1; then
+    export PATH=$PATH:$HOME/.cargo/bin
 fi
 
 
